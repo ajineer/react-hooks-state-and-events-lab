daf51c3535d1d9893c283979001eaf1b9f9f1bf8
Mark <ajineer@outlook.com>
2023-07-03 13:14:36 -0600

    completed lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Item({ name, category }) {
+function Item({ name, category }) { 
+  // const [newCart, setNewCart] = useState([])
+  const [inCart, setinCart] = useState(false)
+  const isInCart = inCart ? "in-cart" : ""
+  const btnText = inCart ? "Remove From Cart" : "Add to Cart"
+  
   return (
-    <li className="">
+    <li className={isInCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={() => addItem(name)} className="add">{btnText}</button>
     </li>
   );
+
+  function addItem(itemName){
+    const newIsInCart = !inCart
+    // let updatedCart
+    
+    // if(inCart === false){
+    //   console.log(inCart)
+    //   updatedCart = [...newCart, itemName]
+    //   setNewCart(updatedCart)
+    // }else{
+    //   updatedCart = [...newCart].filter(item =>{ return item !== itemName})
+    //   setNewCart(updatedCart)
+    // }
+    setinCart(newIsInCart)
+  }
 }
 
 export default Item;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCatagory, setCatagory] = useState("")
+  let filteredItems = []
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={(e)=>{setCatagory(e.target.value)}} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +17,12 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {  
+          (selectedCatagory === "All" || selectedCatagory === "") ? (
+          items.map((item) => (<Item key={item.id} name={item.name} category={item.category} />))):(
+          filteredItems = items.filter(item => item.category === selectedCatagory),
+          filteredItems.map((item) => (<Item key={item.id} name={item.name} category={item.category}/>))
+          )}
       </ul>
     </div>
   );
